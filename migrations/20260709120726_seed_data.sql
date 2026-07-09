9bb0bca895327c6c9473ee2e93de23ac87bbcfa9
nobody <nobody>
2026-07-09 12:11:33 +0300

    seed data

---
+++ migrations/20260709120726_seed_data.sql
@@ -0,0 +1,11 @@
+-- +goose Up
+INSERT INTO auth_user(email, password) VALUES ('test@tashirka.ru', '$2a$10$TWELlyFLjCgXDujqTLYILedtRT.X2.wJaLmCvkdnzPDiK/NUuBQOa');
+INSERT INTO link_link(code, url, clicks, user_id) VALUES ('S9NEfOF', 'https://x.com', 0, 1);
+INSERT INTO link_link(code, url, clicks, user_id) VALUES ('ThZ7rgY', 'https://vk.com', 0, 1);
+INSERT INTO link_link(code, url, clicks, user_id) VALUES ('nVJVrym', 'https://youtube.com', 0, 1);
+INSERT INTO link_link(code, url, clicks, user_id) VALUES ('itUDVt5', 'https://instagram.com', 0, 1);
+INSERT INTO link_link(code, url, clicks, user_id) VALUES ('CbpaPIl', 'https://facebook.com', 0, 1);
+
+-- +goose Down
+DELETE FROM link_link WHERE code IN ('S9NEfOF', 'ThZ7rgY', 'nVJVrym', 'itUDVt5', 'CbpaPIl');
+DELETE FROM auth_user WHERE email = 'test@tashirka.ru';
